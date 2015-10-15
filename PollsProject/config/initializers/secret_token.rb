f71a3d3bf50a5544c7a1be5897cd113cd93df06b
Jake Nelken <jnelks@gmail.com>
2015-10-15 10:07:51 -0400

    Created Rails PollsProject.

---
+++ PollsProject/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollsProject::Application.config.secret_key_base = '22911445f684a1384d4694ddeec52c371f997bab30e2533c83530e779fb4ac80286cb4f232334bdf45a538c1c1d9bf07dd402b96a723a9e1c7ce13fa5d05afa6'
